a15816479f62a4abe39dc24d60b9d0ad74b73efc
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2024-05-27 21:32:22 -0400

    add reviews

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,8 +1,12 @@
-export const Review = ({ title, text }) => {
+export const Review = ({ title, text, text2, rating }) => {
   return (
     <div className="review-container">
-      <div className="title">{title}</div>
+      <div className="title-container">
+        <div className="title">{title}</div>
+        {rating && <div className="rating"> {rating}/10</div>}
+      </div>
       <div>{text}</div>
+      <div className="text2">{text2}</div>
     </div>
   );
 };
