8580630d86b940fc35dad7f777b64c0255c25391
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2024-11-03 17:35:02 -0500

    clean up

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { useState, useRef } from "react";
 import { Review } from "./components/Review";
 import "./index.scss";
 import classNames from "classnames";
-import { tryThis } from "./fetch";
 
 function App() {
   const [showLinks, setShowLinks] = useState(false);
@@ -360,8 +359,6 @@ function App() {
         text2=""
         rating={}
       />  */}
-            <button onClick={tryThis}> hello</button>
-
       <Review 
         title="Anora" 
         director="Sean Baker"
--- src/components/Review.js
+++ src/components/Review.js
@@ -1,17 +1,32 @@
-import { forwardRef } from "react";
+import { forwardRef, useEffect, useState } from "react";
+import { getMovieImage } from "../fetch";
 
 export const Review = forwardRef((props, ref) => {
+  const [imgUrl, setImgUrl] = useState("");
+
+  useEffect(()=> {
+    const getImageWrapper = async () =>{
+      const imgUrlStr = await getMovieImage(props.title);
+      setImgUrl(imgUrlStr)
+    }
+    getImageWrapper();
+
+  },[])
+
   return (
     <div className="review-container" ref={ref}>
       <div className="title-and-date">
         <div className="title-container">
           <div className="title">{props.title}</div>
-          {props.rating && <div className="rating">&nbsp;{props.rating}/10</div>}
+          {props.rating && (
+            <div className="rating">&nbsp;{props.rating}/10</div>
+          )}
         </div>
         <div>{props.date}</div>
         {props.director && <div>Director: {props.director}</div>}
-        {props.releaseDate  &&<div>Released: {props.releaseDate}</div>}
+        {props.releaseDate && <div>Released: {props.releaseDate}</div>}
       </div>
+      {imgUrl && <img src={imgUrl} />}
 
       <div className="text">{props.text}</div>
       {props.text2 && <div className="text2">{props.text2}</div>}
