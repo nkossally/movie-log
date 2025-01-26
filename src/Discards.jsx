13a6e9974d15aa0bbb0299511a94d51f2c7cc3c6
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2025-01-26 10:30:07 -0500

    add pagination

---
+++ src/Discards.jsx
@@ -0,0 +1,43 @@
+const discards = [
+  <Review
+    ref={ref25}
+    title="Witness"
+    text="Fun movie that forces Harrison Ford cop to live with the Amish. Could have done more with the fish out of water concept. The corrupt cops are idiots. [Spoiler] The final moment improves the movie because it evokes lost possibility for a life with the Amish woman, comparing lost love to going to a modern city away from the farm."
+    rating={8}
+  />,
+
+  <Review
+    ref={ref26}
+    title="Silver Linings Playbook"
+    text="Not sure how rom com inevitability of the two coming together can ever be exciting climax. From dance competition onwards, film  drags. I like the montage of Sunday family gatherings at the end. I think of how the film stars likely do not have access and/or do not want “ordinary” suburban domesticity. I do not know suburban ordinary family life either."
+    rating={8}
+  />,
+
+  <Review
+    ref={ref28}
+    title="Are You There God? It’s Me, Margaret"
+    text="Cute. Slight. I like seeing a film from a child’s perspective. I believe children think they are the center of the universe, but I don’t think that Margaret is the center of the lives of the adults. I believe that the grandparents would continue arguing about whether she should be Jewish or Christian even after she stormed off."
+    rating={6}
+  />,
+
+  <Review
+    ref={ref35}
+    title="The Age of Innocence"
+    text="[Spoiler] Felt so bad watching DDL retain Michelle Pfeiffer as an idea of a life he gave up decades prior instead of doing the modern thing of taking up a second wife. Are other people ideas? Makes me regret wasting my own life. I feel like Saoirse Ronan would have been scarier and steelier in the Wynona Ryder part. Ryder should have been nominated for Girl Interrupted. Not for this."
+    rating={9}
+  />,
+
+  <Review
+    ref={ref36}
+    title="Sea of Love"
+    text="[Spoiler] Some imagery that is reminiscent of pegging (when the killer is assumed female) and gay sex (when killer revealed to be man). Erotic thriller. Stalking. Obsession. Cop falls for woman who seems to drive men to murder. Ellen Barkin believably/charmingly working class chick from New York."
+    rating={7}
+  />,
+
+  <Review
+    ref={ref41}
+    title="A Man for All Seasons"
+    text="Smart historical movie based on play about Thomas More’s refusal to approve of Henry VIII’s divorce. Play is probably better at teasing apart notions of law, justice, principle, the king, pragmatism. Perfect but loses a point for lack of levity, stunning visuals and sex. "
+    rating={9}
+  />,
+];
