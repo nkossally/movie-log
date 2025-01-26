a3ba5b52b9c5afa38f9266b90624b16d9de57dc0
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2025-01-26 11:13:40 -0500

    finish pagination

--- src/App.js
+++ src/App.js
@@ -12,12 +12,15 @@ function App() {
   const [page, setPage] = useState(1)
 
   const decreasePage = () =>{
-    setPage(page - 1)
+    setTimeout(()=>{
+      setPage(page - 1)
+    }, 200)
   }
 
   const increasePage = () =>{
-    console.log("pizza")
-    setPage(page + 1)
+    setTimeout(()=>{
+      setPage(page + 1)
+    }, 200)  
   }
 
   const ref1 = useRef(null);
@@ -140,8 +143,9 @@ function App() {
     setShowLinks(!showLinks);
   };
 
-  const scrollRefIntoView = (ref) => {
+  const scrollRefIntoView = (ref, destinationPage) => {
     return () => {
+      setPage(destinationPage)
       setTimeout(() => {
         ref.current?.scrollIntoView({ behavior: "smooth" });
       }, 10);
@@ -1115,336 +1119,116 @@ function App() {
   ]
 
   const reviewLinks = [
-    <button className="title-link" onClick={scrollRefIntoView(ref115)}>
-      10 Things I Hate About You
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref114)}>
-      Hard Truths
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref113)}>
-      The Brutalist
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref112)}>
-      Sorry To Bother You
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref111)}>
-      Mission Impossible III
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref110)}>
-      The Man Who Knew Too Much
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref109)}>
-      Nosferatu
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref108)}>
-      Mission impossible II
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref107)}>
-      North by Northwest
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref106)}>
-      The Cable Guy
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref105)}>
-      Velvet Goldmine
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref104)}>
-      Red Eye
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref103)}>
-      The Good Liar
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref102)}>
-      Clueless
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref101)}>
-      Ed Wood
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref100)}>
-      A Simple Plan
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref99)}>
-      Dune 2
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref98)}>
-      Dune
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref97)}>
-      Gladiator II
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref96)}>
-      Face/Off
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref95)}>
-      Come Back to the Five and Dime, Jimmy Dean, Jimmy Dean
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref94)}>
-      A Real Pain
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref93)}>
-      Apollo 13
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref92)}>
-      The Long Kiss Goodnight
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref91)}>
-      Unforgiven
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref90)}>
-      Almost Famous
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref89)}>
-      Queer
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref88)}>
-      Juror # 2
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref87)}>
-      Conclave
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref86)}>
-      Husbands and Wives
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref85)}>
-      My Cousin Vinny
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref84)}>
-      Wicked
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref83)}>
-      Scream VI
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref82)}>
-      Emilia Perez
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref81)}>
-      While You Were Sleeping
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref80)}>
-      The Last Days of Disco
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref79)}>
-      The Rainmaker
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref78)}>
-      The Firm
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref77)}>
-      Anora
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref76)}>
-      The Post
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref75)}>
-      Dick
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref74)}>
-      Bachelorette
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref73)}>
-      American President
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref72)}>
-      Primary Colors
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref71)}>
-      Rivals (TV)
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref70)}>
-      Generation Kill (TV)
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref69)}>
-      Fallout (TV)
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref68)}>
-      Baby Reindeer (TV)
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref67)}>
-      Shogun (TV)
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref66)}>
-      Howards End
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref65)}>
-      Strange Darling
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref64)}>
-      Longlegs
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref63)}>
-      Remains of the Day
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref62)}>
-      Let Them All Talk
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref61)}>
-      The Ref
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref60)}>
-      Jerry Maguire
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref59)}>
-      The Pelican Brief
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref58)}>
-      Haywire
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref57)}>
-      Bad Boys: Ride or Die
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref56)}>
-      Lincoln Lawyer
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref55)}>
-      The Knick Season 1
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref54)}>
-      Ocean’s Twelve
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref53)}>
-      Pride and Glory
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref52)}>
-      The Bourne Ultimatum
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref51)}>
-      The Three Faces of Eve
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref50)}>
-      Silence
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref49)}>
-      The Verdict
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref48)}>
-      Shakespeare in Love
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref47)}>
-      Hit Man
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref46)}>
-      Elizabeth
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref45)}>
-      Central Station
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref44)}>
-      The Bourne Supremacy
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref43)}>
-      The Bourne Identity
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref42)}>
-      Superbad
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref41)}>
-      The Batman
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref1)}>
-      Reacher
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref2)}>
-      Logan Lucky
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref3)}>
-      Femme
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref4)}>
-      I Care a Lot
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref5)}>
-      Mission Impossible Dead Reckoning
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref6)}>
-      Fabulous Baker Boys
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref7)}>
-      Death Becomes Her
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref8)}>
-      The Challengers
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref9)}>
-      Devil Wears Prada
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref10)}>
-      eXistenZ
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref11)}>
-      Constantine
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref12)}>
-      Dewey Cox
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref13)}>
-      Last Duel
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref14)}>
-      Before the Devil Knows Your Dead
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref15)}>
-      Eastern Promises
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref16)}>
-      Dredd
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref17)}>
-      Civil War
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref18)}>
-      Speed
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref19)}>
-      Talented Mr. Ripley
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref20)}>
-      Videodrome
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref21)}>
-      Southland Tales
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref22)}>
-      Miss Congeniality
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref23)}>
-      Rounders
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref24)}>
-      American Gigolo
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref27)}>
-      The Quiet Girl
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref29)}>
-      The Holdovers
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref30)}>
-      Godzilla Minus One
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref31)}>
-      Clemency
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref32)}>
-      The Usual Suspects
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref33)}>
-      Phantom Thread
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref34)}>
-      Triple Frontier
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref37)}>
-      Barfly
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref38)}>
-      Jaws
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref39)}>
-      Devil’s Advocate
-    </button>,
-    <button className="title-link" onClick={scrollRefIntoView(ref40)}>
-      Ocean’s 11
-    </button>,
+    [ref115, "10 Things I Hate About You"],
+    [ref114, "Hard Truths"],
+    [ref113, "The Brutalist"],
+    [ref112, "Sorry To Bother You"],
+    [ref111, "Mission Impossible III"],
+    [ref110, "The Man Who Knew Too Much"],
+    [ref109, "Nosferatu"],
+    [ref108, "Mission impossible II"],
+    [ref107, "North by Northwest"],
+    [ref106, "The Cable Guy"],
+    [ref105, "Velvet Goldmine"],
+    [ref104, "Red Eye"],
+    [ref103, "The Good Liar"],
+    [ref102, "Clueless"],
+    [ref101, "Ed Wood"],
+    [ref100, "A Simple Plan"],
+    [ref99, "Dune 2"],
+    [ref98, "Dune"],
+    [ref97, "Gladiator II"],
+    [ref96, "Face/Off"],
+    [ref95, "Come Back to the Five and Dime, Jimmy Dean, Jimmy Dean"],
+    [ref94, "A Real Pain"],
+    [ref93, "Apollo 13"],
+    [ref92, "The Long Kiss Goodnight"],
+    [ref91, "Unforgiven"],
+    [ref90, "Almost Famous"],
+    [ref89, "Queer"],
+    [ref88, "Juror # 2"],
+    [ref87, "Conclave"],
+    [ref86, "Husbands and Wives"],
+    [ref85, "My Cousin Vinny"],
+    [ref84, "Wicked"],
+    [ref83, "Scream VI"],
+    [ref82, "Emilia Perez"],
+    [ref81, "While You Were Sleeping"],
+    [ref80, "The Last Days of Disco"],
+    [ref79, "The Rainmaker"],
+    [ref78, "The Firm"],
+    [ref77, "Anora"],
+    [ref76, "The Post"],
+    [ref75, "Dick"],
+    [ref74, "Bachelorette"],
+    [ref73, "American President"],
+    [ref72, "Primary Colors"],
+    [ref71, "Rivals (TV)"],
+    [ref70, "Generation Kill (TV)"],
+    [ref69, "Fallout (TV)"],
+    [ref68, "Baby Reindeer (TV)"],
+    [ref67, "Shogun (TV)"],
+    [ref66, "Howards End"],
+    [ref65, "Strange Darling"],
+    [ref64, "Longlegs"],
+    [ref63, "Remains of the Day"],
+    [ref62, "Let Them All Talk"],
+    [ref61, "The Ref"],
+    [ref60, "Jerry Maguire"],
+    [ref59, "The Pelican Brief"],
+    [ref58, "Haywire"],
+    [ref57, "Bad Boys: Ride or Die"],
+    [ref56, "Lincoln Lawyer"],
+    [ref55, "The Knick Season 1"],
+    [ref54, "Ocean’s Twelve"],
+    [ref53, "Pride and Glory"],
+    [ref52, "The Bourne Ultimatum"],
+    [ref51, "The Three Faces of Eve"],
+    [ref50, "Silence"],
+    [ref49, "The Verdict"],
+    [ref48, "Shakespeare in Love"],
+    [ref47, "Hit Man"],
+    [ref46, "Elizabeth"],
+    [ref45, "Central Station"],
+    [ref44, "The Bourne Supremacy"],
+    [ref43, "The Bourne Identity"],
+    [ref42, "Superbad"],
+    [ref41, "The Batman"],
+    [ref1, "Reacher"],
+    [ref2, "Logan Lucky"],
+    [ref3, "Femme"],
+    [ref4, "I Care a Lot"],
+    [ref5, "Mission Impossible Dead Reckoning"],
+    [ref6, "Fabulous Baker Boys"],
+    [ref7, "Death Becomes Her"],
+    [ref8, "The Challengers"],
+    [ref9, "Devil Wears Prada"],
+    [ref10, "eXistenZ"],
+    [ref11, "Constantine"],
+    [ref12, "Dewey Cox"],
+    [ref13, "Last Duel"],
+    [ref14, "Before the Devil Knows Your Dead"],
+    [ref15, "Eastern Promises"],
+    [ref16, "Dredd"],
+    [ref17, "Civil War"],
+    [ref18, "Speed"],
+    [ref19, "Talented Mr. Ripley"],
+    [ref20, "Videodrome"],
+    [ref21, "Southland Tales"],
+    [ref22, "Miss Congeniality"],
+    [ref23, "Rounders"],
+    [ref24, "American Gigolo"],
+    [ref27, "The Quiet Girl"],
+    [ref29, "The Holdovers"],
+    [ref30, "Godzilla Minus One"],
+    [ref31, "Clemency"],
+    [ref32, "The Usual Suspects"],
+    [ref33, "Phantom Thread"],
+    [ref34, "Triple Frontier"],
+    [ref37, "Barfly"],
+    [ref38, "Jaws"],
+    [ref39, "Devil’s Advocate"],
+    [ref40, "Ocean’s 11"],
   ];
 
   const maxPage = Math.floor(reviews.length / 20) + 1
@@ -1460,9 +1244,13 @@ function App() {
       {showLinks && (
         <div className="links-container">
           {reviewLinks.map((reviewLink, idx)=>{
-            if(idx >= (page -1) * REVIEWS_PER_PAGE && idx < (page * REVIEWS_PER_PAGE)){
-              return reviewLink
-            }
+            const ref = reviewLink[0]
+            const title = reviewLink[1]
+            const destinationPage = Math.floor(idx / REVIEWS_PER_PAGE) + 1
+            return     <button className="title-link" onClick={scrollRefIntoView(ref, destinationPage)}>
+                {title}
+                </button>
+            
           })}
 
         </div>
