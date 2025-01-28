33210001fecbe230fbc4ebf910eb94d3bb68eb79
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2025-01-28 08:34:42 -0500

    add paragraphs prop to Review and edit review of The Firm

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,7 +1,7 @@
 import { forwardRef, useEffect, useState } from "react";
 import { getMovieImage } from "../fetch";
 import { Divider } from "./Divider";
-import { Text} from "./Text"
+import { Text } from "./Text";
 import classNames from "classnames";
 
 export const Review = forwardRef((props, ref) => {
@@ -15,7 +15,8 @@ export const Review = forwardRef((props, ref) => {
     getImageWrapper();
   }, []);
 
-  const hasMultipleWriters = props.writer && props.writer.split("").includes(",")
+  const hasMultipleWriters =
+    props.writer && props.writer.split("").includes(",");
 
   return (
     <div className="review-container" ref={ref}>
@@ -32,33 +33,62 @@ export const Review = forwardRef((props, ref) => {
           <div className="movie-data">
             <div>{props.date}</div>
             {props.director && <div>Director: {props.director}</div>}
-            {props.writer && <div> {`Writer${hasMultipleWriters ? "s" : ""}: ${props.writer}`}</div>}
+            {props.writer && (
+              <div>
+                {" "}
+                {`Writer${hasMultipleWriters ? "s" : ""}: ${props.writer}`}
+              </div>
+            )}
             {props.releaseDate && <div>Released: {props.releaseDate}</div>}
             {props.theater && <div>Theater: {props.theater}</div>}
           </div>
         </div>
         {imgUrl && <img className="movie-poster" src={imgUrl} />}
       </div>
-
-      <div className="text"><Text text={props.text} /></div>
+      {props.text && (
+        <div className="text">
+          <Text text={props.text} />
+        </div>
+      )}
       {props.text2 && (
         <>
           <br />
-          <div className="text2"><Text text={props.text2} /></div>
+          <div className="text2">
+            <Text text={props.text2} />
+          </div>
         </>
       )}
       {props.text3 && (
         <>
           <br />
-          <div className="text2"><Text text={props.text3} /></div>
+          <div className="text2">
+            <Text text={props.text3} />
+          </div>
         </>
       )}
       {props.text4 && (
         <>
           <br />
-          <div className="text2"><Text text={props.text4} /></div>
+          <div className="text2">
+            <Text text={props.text4} />
+          </div>
         </>
       )}
+      {props.paragraphs && (
+        <>
+          {props.paragraphs.map((paragraph, idx) => {
+            return (
+              <>
+                {idx > 0 && <br />}
+                <div className="text2">
+                  <Text text={paragraph} />
+                </div>
+              </>
+            );
+          })}
+        </>
+      )}
+
       <Divider />
     </div>
   );
